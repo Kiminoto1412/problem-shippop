15c3a2a36eab208175625f3e87553cff3da5e200
paruj_lab <paruj.lab@gmail.com>
2022-08-25 17:48:47 +0700

    add:prblem

---
+++ problem-shippop.js
@@ -0,0 +1,31 @@
+// 1.1)
+// function introduction(firstName, lastName) {
+//   let result;
+//   if (firstName === "Shippop") {
+//     result = "Best Online Shipping Platform";
+//   } else {
+//     result = `Hello Shippop, My name is ${firstName} ${lastName}`;
+//   }
+//   return result;
+// }
+
+// console.log(introduction("Mam", "Lab"));
+// console.log(introduction("Shippop"));
+
+// 1.2)
+// วิธีแรกใช้ JSON.parse(JSON.stringify(arr))
+// ตัวอย่าง
+// var arr = [{'obj1':1}, {'obj2':2}]
+// var clone = JSON.parse(JSON.stringify(arr));
+// console.log(clone);
+
+// วิธีสองใช้ spread operator
+// ตัวอย่าง
+// var arr = [{'obj1':1}, {'obj2':2}]
+// var clone = [...arr]
+// console.log(clone);
+
+// 2.Database Design
+// databaseที่เคยทำมาจะdesign ด้วยer-diagram และ ใช้web draw.ioในการวาด ด้วยหลักการMVC
+// ปัญหาผมคิดว่าเป็นเรื่องการออกแบบdatabase ของprojectใหม่ๆที่ยังไม่เคยทำว่าจะออกแบบประมาณไหนดี
+// วิธีแก้ ผมจะsearch หาตัวอย่างจากในinternet และถามจากผู้รู้ว่าสิ่งที่ผมคิดมันดีมั้ย
